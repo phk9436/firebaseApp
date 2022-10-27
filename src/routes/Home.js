6cd547eed628f7882229d758c6d84e603aacc4bf
digitalworks_hgpark <phk9436@naver.com>
2022-10-27 11:15:56 +0900

    [Feat] 트윗 작성 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,23 @@
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import React, {useState} from 'react';
+import { async } from '@firebase/util';
+import { addDoc, collection, serverTimestamp, getDocs, query } from 'firebase/firestore';
+import React, {useEffect, useState} from 'react';
 import { dbService } from '../firebase';
 
 function Home() {
   const [nweet, setNweet] = useState("");
+  const [nweetLi, setNweetLi] = useState([]);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const getNweets = async() => {
+    setNweetLi([]);
+    const queryList = await getDocs(query(collection(dbService, "nweets")));
+    queryList.forEach((docs) => {
+      const nweetObj = {...docs.data() , id:docs.id}
+      setNweetLi(prev => [nweetObj, ...prev])
+    })
+  }
+  useEffect(()=> {
+    getNweets();
+  }, [isSubmit])
   const onSubmit = async(e) => {
     e.preventDefault();
     try{
@@ -16,6 +30,7 @@ function Home() {
     } catch(err) {
       console.log(err.message);
     }
+    setIsSubmit((prev) => !prev);
   }
   const onChange = (e) => {
     setNweet(e.target.value)
@@ -25,6 +40,13 @@ function Home() {
       <form action="" onSubmit={onSubmit}>
         <input type="text" placeholder="What's on your mind?" maxLength={120} onChange={onChange} value={nweet}/>
         <button type='submit'>Nweet</button>
+        <div>
+          {nweetLi.map((el) => {
+            return (
+            <h4 key={el.id}>{el.nweet}</h4>
+            )
+          })}
+        </div>
       </form>
     </div>
   );
